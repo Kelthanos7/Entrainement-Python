b118d8e5ca686a7fbb3b73ae3de7e0626e1a2b9a
Carlo Circo <Carlo.Circo@outlook.fr>
2026-03-24 16:21:55 +0100

    Exo comparaison

---
+++ Comparaison-de-deux-nombres.py
@@ -0,0 +1,8 @@
+a = int(input("Saisissez un nombre a : "))
+b = int(input("Saisissez un nombre b : "))
+if a == b:
+    print("Ils sont égaux.")
+elif a > b:
+    print("Le premier est plus grand")
+elif b > a:
+    print("Le deuxième est plus grand")
